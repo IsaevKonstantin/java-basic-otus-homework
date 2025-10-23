5fc928c7026ef6ff0b28d07af860ce60dd793cdf
IsaevKA <kostik.isaev.97@list.ru>
2025-10-23 22:46:17 +0300

    hw13 is done

--- src/main/java/ru/otus/java/basic/hw13/server/CalcServer.java
+++ src/main/java/ru/otus/java/basic/hw13/server/CalcServer.java
@@ -13,6 +13,14 @@ import java.util.List;
 import java.util.Map;
 
 public class CalcServer {
+    private static final List<String> OPS = Arrays.asList("*", "/", "+", "-");
+    private static final Map<String, Integer> PRIORITY = Map.of(
+            "*", 2,
+            "/", 2,
+            "+", 1,
+            "-", 1
+    );
+
     public static void main(String[] args) {
         System.out.println("\nСервер калькулятора начал работу! :)");
         while (true) {
@@ -48,9 +56,14 @@ public class CalcServer {
         }
     }
 
-    public static double evaluate(String expression) throws DataException {
+    private static double evaluate(String expression) throws DataException {
         System.out.println("\nСервер калькулятора начал считать выражение...");
         List<String> tokens = new ArrayList<>(Arrays.asList(expression.trim().split("\\s+")));
+        resolveParentheses(tokens);
+        return calculateFlat(tokens);
+    }
+
+    private static void resolveParentheses(List<String> tokens) throws DataException {
         while (tokens.contains("(")) {
             int open = -1;
             int close = -1;
@@ -67,61 +80,69 @@ public class CalcServer {
             }
             List<String> subExpr = new ArrayList<>(tokens.subList(open + 1, close));
             double subResult = calculateFlat(subExpr);
-            for (int i = close; i >= open; i--) {
-                tokens.remove(i);
-            }
-            tokens.add(open, String.valueOf(subResult));
+            replaceSubExpression(tokens, open, close, subResult);
         }
-        return calculateFlat(tokens);
+    }
+
+    private static void replaceSubExpression(List<String> tokens, int open, int close, double value) {
+        for (int i = close; i >= open; i--) {
+            tokens.remove(i);
+        }
+        tokens.add(open, String.valueOf(value));
     }
 
     private static double calculateFlat(List<String> tokens) throws DataException {
         try {
-            List<String> ops = Arrays.asList("*", "/", "+", "-");
-            Map<String, Integer> priority = Map.of(
-                    "*", 2,
-                    "/", 2,
-                    "+", 1,
-                    "-", 1
-            );
             for (int p = 2; p >= 1; p--) {
-                int i = 0;
-                while (i < tokens.size()) {
-                    String token = tokens.get(i);
-                    if (i % 2 != 0 && !(token.equals("+") || token.equals("-") || token.equals("/") || token.equals("*")))
-                        throw new DataException("Неизвестная операция (Допустимые операторы: +,-,*,/)! Проверьте выражение!");
-                    if (ops.contains(token) && priority.get(token) == p) {
-                        double a = Double.parseDouble(tokens.get(i - 1));
-                        double b = Double.parseDouble(tokens.get(i + 1));
-                        double res;
-                        switch (token) {
-                            case "+":
-                                res = a + b;
-                                break;
-                            case "-":
-                                res = a - b;
-                                break;
-                            case "*":
-                                res = a * b;
-                                break;
-                            case "/":
-                                if (b == 0) throw new DataException("Деление на 0! Проверьте выражение!");
-                                res = a / b;
-                                break;
-                            default:
-                                throw new DataException("Неизвестная операция! Проверьте выражение!");
-                        }
-                        tokens.set(i - 1, String.valueOf(res));
-                        tokens.remove(i);
-                        tokens.remove(i);
-                    } else {
-                        i++;
-                    }
-                }
+                processOperations(tokens, p);
             }
             return Double.parseDouble(tokens.get(0));
         } catch (NumberFormatException | NullPointerException | IndexOutOfBoundsException e) {
             throw new DataException("Не удалось преобразовать введенные данные! Проверьте выражение!");
         }
+    }
+
+    private static void processOperations(List<String> tokens, int priorityLevel) throws DataException {
+        int i = 0;
+        while (i < tokens.size()) {
+            String token = tokens.get(i);
+            if (isInvalidOperatorPosition(i, token))
+                throw new DataException("Неизвестная операция (Допустимые операторы: +,-,*,/)! Проверьте выражение!");
+            if (isMatchingPriority(token, priorityLevel)) {
+                double res = performOperation(tokens, i, token);
+                updateTokens(tokens, i, res);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private static boolean isInvalidOperatorPosition(int i, String token) {
+        return i % 2 != 0 && !OPS.contains(token);
+    }
+
+    private static boolean isMatchingPriority(String token, int p) {
+        return OPS.contains(token) && PRIORITY.get(token) == p;
+    }
+
+    private static double performOperation(List<String> tokens, int i, String op) throws DataException {
+        double a = Double.parseDouble(tokens.get(i - 1));
+        double b = Double.parseDouble(tokens.get(i + 1));
+        switch (op) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/":
+                if (b == 0) throw new DataException("Деление на 0! Проверьте выражение!");
+                return a / b;
+            default:
+                throw new DataException("Неизвестная операция! Проверьте выражение!");
+        }
+    }
+
+    private static void updateTokens(List<String> tokens, int i, double result) {
+        tokens.set(i - 1, String.valueOf(result));
+        tokens.remove(i);
+        tokens.remove(i);
     }
 }
